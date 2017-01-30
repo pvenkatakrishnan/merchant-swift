2fd80ad170ab06e64c1324aafc01fccb23a8ff33
poornima <pvenkatakrishnan@paypal.com>
2017-01-30 10:22:21 -0800

    Initial Commit

---
+++ merchant-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  merchant-swift
+//
+//  Created by Venkatakrishnan,Poornima on 1/30/17.
+//  Copyright © 2017 Venkatakrishnan,Poornima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
